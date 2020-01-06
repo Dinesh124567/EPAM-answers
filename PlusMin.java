706f21655f24839a766bca9f8d41c83b1f9dd478
Dinesh coder <51782451+Dinesh124567@users.noreply.github.com>
2020-01-06 20:55:17 +0530

    Add files via upload

---
+++ PlusMin.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PlusMin{
+   public static void main(String []ar){
+       Scanner s=new Scanner(System.in);
+       if(backtr(0,0,"", Integer.toString(s.nextInt())));
+       else System.out.println("not possible");
+}
+
+   public static boolean backtr(int sum, int pos, String str, String num){
+         String t="";
+                 for(int i=0; i<num.length(); i++) t+="-";
+         if(sum==0 && pos==num.length())
+             { if(str.charAt(0)=='+')System.out.println(str.substring(1,str.length())); return true;}             
+
+  
+        else if(pos<num.length()){
+             if(backtr(sum+Integer.parseInt(Character.toString(num.charAt(pos))), pos+1, str+"+", num))return true;
+             if(backtr(sum-Integer.parseInt(Character.toString(num.charAt(pos))), pos+1, str+"-", num))return true; 
+             }
+       
+        return false;
+
+      
+}}
